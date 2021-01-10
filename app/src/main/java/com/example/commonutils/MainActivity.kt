a66b836e9691d3be6f49e8a422c3dd912c68265c
1685129336 <1685129336@qq.com>
2021-01-10 19:54:07 +0800

    修改了无法在fragment内部调用方法的bug

--- app/src/main/java/com/example/commonutils/MainActivity.kt
+++ app/src/main/java/com/example/commonutils/MainActivity.kt
@@ -2,20 +2,12 @@ package com.example.commonutils
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import com.example.common.picture.MPictureUtil
-import core.api.FragmentPassByValue
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    }
-
-    class MyFragment:Fragment(),FragmentPassByValue{
-        override fun send(data: Bundle?) {
-
-        }
     }
 }
