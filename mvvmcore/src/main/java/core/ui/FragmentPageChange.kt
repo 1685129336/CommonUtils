23538820ebc8407b7387a5d8745478adc3a36961
1685129336 <1685129336@qq.com>
2021-01-11 09:54:55 +0800

    修改跳转fragment的逻辑

--- mvvmcore/src/main/java/core/ui/FragmentPageChange.kt
+++ mvvmcore/src/main/java/core/ui/FragmentPageChange.kt
@@ -1,17 +1,26 @@
 package core.ui
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import core.api.FragmentPassByValue
 
-fun FragmentPassByValue.startFragment(activity:BaseActivityPageChangeFragment, fragment: Class<*>, data: Bundle?){
+fun FragmentPassByValue.startFragment(activity: AppCompatActivity, fragment: Class<*>, data: Bundle?){
+    if(activity is BaseActivityPageChangeFragment)
     activity.startFragment(fragment, data)
+    else Throwable("跳转fragment activity必须是BaseActivityPageChangeFragment的子类")
 }
-fun FragmentPassByValue.startFragment(activity:BaseActivityPageChangeFragment, fragment: Class<*>, data: Bundle?, animIn:Int, animOut:Int){
+fun FragmentPassByValue.startFragment(activity:AppCompatActivity, fragment: Class<*>, data: Bundle?, animIn:Int, animOut:Int){
+    if(activity is BaseActivityPageChangeFragment)
     activity.startFragment(fragment, data,animIn, animOut)
+    else Throwable("跳转fragment activity必须是BaseActivityPageChangeFragment的子类")
 }
-fun FragmentPassByValue.callbackFragment(activity:BaseActivityPageChangeFragment):Boolean{
-    return activity.callbackFragment()
+fun FragmentPassByValue.callbackFragment(activity:AppCompatActivity):Boolean{
+   return if(activity is BaseActivityPageChangeFragment)
+     activity.callbackFragment()
+    else false
 }
-fun FragmentPassByValue.callbackFragment(activity:BaseActivityPageChangeFragment, animIn: Int, animOut: Int):Boolean{
-    return activity.callbackFragment(animIn, animOut)
+fun FragmentPassByValue.callbackFragment(activity:AppCompatActivity, animIn: Int, animOut: Int):Boolean{
+    return if(activity is BaseActivityPageChangeFragment)
+    activity.callbackFragment(animIn, animOut)
+    else false
 }
